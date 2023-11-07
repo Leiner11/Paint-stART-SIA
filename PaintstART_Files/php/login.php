f18511fb510d45481b7ea069ac244cde6eab4b3d
megalosans <ariadoreniel@gmail.com>
2023-11-07 21:07:22 +0800

    updates

--- PaintstART_Files/php/login.php
+++ PaintstART_Files/php/login.php
@@ -1,18 +1,18 @@
 <?php
 	// Connect to the database
 	$host = 'localhost';  // replace with your database host
-	$dbname = 'userregistration'; // replace with your database name
+	$dbname = 'users'; // replace with your database name
 	$username = 'Group4PS_Admin'; // replace with your database username
 	$password = 'group_4_PS!!!1111'; // replace with your database password
 	$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-	// Get the user's entered username and password
-	$username = $_POST['username'];
+	// Get the user's entered email and password
+	$username = $_POST['email'];
 	$password = $_POST['password'];
 
 	// Query the database for the user's details
-	$stmt = $db->prepare("SELECT * FROM register WHERE username=:username AND password=:password");
-	$stmt->bindValue(':username', $username);
+	$stmt = $db->prepare("SELECT * FROM user_profile WHERE email=:email AND password=:password");
+	$stmt->bindValue(':email', $username);
 	$stmt->bindValue(':password', $password);
 	$stmt->execute();
 
--- PaintstART_Files/php/register.php
+++ PaintstART_Files/php/register.php
@@ -1,19 +1,19 @@
 <?php
-			$name = $_POST['name'];
 			$username = $_POST['username'];
+			$email = $_POST['email'];
 			$password = $_POST['password'];
 
 				//Connect to database
 				$servername = "localhost";
 				$dbusername = "Group4PS_Admin";
 				$dbpassword = "group_4_PS!!!1111";
-				$dbname = "userregistration";
+				$dbname = "users";
 				$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 				if ($conn->connect_error) {
 				    die("Connection failed: " . $conn->connect_error);
 				}
 				//Insert user data into database
-				$sql = "INSERT INTO register (name, username, password) VALUES ('$name', '$username', '$password')";
+				$sql = "INSERT INTO user_profile (username, email, password) VALUES ('$username', '$email', '$password')";
 				if ($conn->query($sql) === TRUE) {
 				    echo "New record created successfully";
 				} else {
